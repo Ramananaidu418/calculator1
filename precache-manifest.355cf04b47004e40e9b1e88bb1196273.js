09079c2de5bf8197f42af5ad98ea2bb9e62937cb
Ramananaidu418 <mvr418@gmail.com>
2020-09-28 15:26:16 +0530

    Updates

---
+++ precache-manifest.355cf04b47004e40e9b1e88bb1196273.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac675acd29671d9cc6a5371598fa87e",
+    "url": "/calculator1/index.html"
+  },
+  {
+    "revision": "0a854a3144491700b1e6",
+    "url": "/calculator1/static/css/main.729dfcbb.chunk.css"
+  },
+  {
+    "revision": "33a76aeee2f72144865a",
+    "url": "/calculator1/static/js/2.6fe875b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator1/static/js/2.6fe875b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a854a3144491700b1e6",
+    "url": "/calculator1/static/js/main.b46234c5.chunk.js"
+  },
+  {
+    "revision": "df7edc5942d36a27d873",
+    "url": "/calculator1/static/js/runtime-main.c63b4be9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator1/static/media/logo.5d5d9eef.svg"
+  }
+]);
